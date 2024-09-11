f8796c18aa4f830ab40197e5cde30384bdeb9e66
jhony.benitez@tigo.net.py <jhonyben.94@gmail.com>
2024-09-10 20:39:54 -0400

    Entrega de parcial 10/09

---
+++ src/main/java/py/edu/unasur/models/resutls/ResultadoEncontrarValores.java
@@ -0,0 +1,32 @@
+package py.edu.unasur.models.resutls;
+
+public class ResultadoEncontrarValores {
+    Integer menor;
+    Integer mayor;
+    Integer promedio;
+
+    public Integer getMenor() {
+        return menor;
+    }
+
+    public void setMenor(Integer menor) {
+        this.menor = menor;
+    }
+
+    public Integer getMayor() {
+        return mayor;
+    }
+
+    public void setMayor(Integer mayor) {
+        this.mayor = mayor;
+    }
+
+    public Integer getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(Integer promedio) {
+        this.promedio = promedio;
+    }
+
+}
---
+++ src/main/java/py/edu/unasur/resources/ParcialResource.java
@@ -0,0 +1,46 @@
+package py.edu.unasur.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import py.edu.unasur.models.resutls.ResultadoEncontrarValores;
+
+@Path("/parcial") // /parcial
+public class ParcialResource {
+    @GET
+    @Path("/suma/{numero1}/{numero2}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer sumaDeDosNumeros(
+            @PathParam("numero1") Integer numero1,
+            @PathParam("numero2") Integer numero2) {
+        return numero1 + numero2;
+    }
+
+    @GET
+    @Path("encontrar-valores")
+    public ResultadoEncontrarValores encontrarValoresEnLista() {
+        List<Integer> numeros = Arrays.asList(12, 31, 1, 2, 31, 1, 123);
+        ResultadoEncontrarValores respuesta = new ResultadoEncontrarValores();
+        respuesta.setMayor(0);
+        respuesta.setMenor(250);
+        Integer sumatoria = 0;
+        for (Integer valor : numeros) {
+            if (respuesta.getMayor() < valor) {
+                respuesta.setMayor(valor);
+            }
+            if (respuesta.getMenor() > valor) {
+                respuesta.setMenor(valor);
+            }
+            sumatoria += valor;
+        }
+        Integer valorPromedio = sumatoria / numeros.size();
+        respuesta.setPromedio(valorPromedio);
+
+        return respuesta;
+    }
+}
